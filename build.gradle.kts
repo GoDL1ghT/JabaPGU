c2996462b5ee617fcd55096371090a99f173284d
GoDL1ghT <sokolovglebik@gmail.com>
2024-10-14 22:56:23 +0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+import org.gradle.internal.impldep.com.jcraft.jsch.ConfigRepository.defaultConfig
+
+plugins {
+    id("java")
+}
+
+group = "ru.godl1ght"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc> {
+    options.encoding = "UTF-8"
+}
